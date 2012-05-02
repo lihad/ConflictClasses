fe04d29187b2069572d6bcae7a00189779e8e70c
Lihad <kylecubed@comcast.net>
2012-05-02 13:16:31 -0700

    BlackMage Abilities

--- src/Lihad/ConflictClasses/ClassListeners/BlackMageListener.java
+++ src/Lihad/ConflictClasses/ClassListeners/BlackMageListener.java
@@ -1,7 +1,18 @@
 package Lihad.ConflictClasses.ClassListeners;
 
-import org.bukkit.event.Listener;
+import java.util.List;
 
+import org.bukkit.Effect;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import Lihad.ConflictClasses.Classes;
 import Lihad.ConflictClasses.ConflictClasses;
 
 public class BlackMageListener implements Listener {
@@ -9,4 +20,21 @@ public class BlackMageListener implements Listener {
 	public BlackMageListener(ConflictClasses instance) {
 		plugin = instance;
 	}
+	@EventHandler
+	public static void onPlayerInteract(PlayerInteractEvent event){
+		if(ConflictClasses.PLAYER_CURRENT_INDEX.get(event.getPlayer().getName()) == Classes.BLACK_MAGE && event.getPlayer().getItemInHand() != null
+				&& event.getPlayer().getItemInHand().getType() == Material.BOOK && (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)){
+			List<Block> blocks = event.getPlayer().getLineOfSight(null, 5);
+			List<Entity> entities = event.getPlayer().getNearbyEntities(10, 10, 10);
+			for(int i = 0;i<blocks.size();i++){
+				blocks.get(i).getWorld().playEffect(blocks.get(i).getLocation(), Effect.MOBSPAWNER_FLAMES, null);
+				for(int j = 0;j<entities.size();j++){
+					if(entities.get(j) instanceof LivingEntity && (blocks.get(i).getLocation().equals(((LivingEntity)entities.get(j)).getLocation().getBlock().getLocation())
+							|| blocks.get(i).getLocation().equals(((LivingEntity)entities.get(j)).getEyeLocation().getBlock().getLocation()))){
+						entities.get(j).setFireTicks(60);
+					}
+				}
+			}
+		}
+	}  
 }
